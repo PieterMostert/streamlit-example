3e86fdf96527cccb61205bb8556b4dc660f8bb6e
Pieter Mostert <pi.mostert@gmail.com>
2022-03-20 17:07:55 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,14 +14,38 @@ forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
-materials = {}
+st.session_state.selected_materials = {}
+st.session_state.selected_oxides = {}
 
-def add_material(material_id, container):
+def add_material(n):
+    material_id = st.session_state['Material {}.format(n)]
     with requests.Session() as session:
         get_url = 'https://glazy.org/api/recipes/{}'.format(material_id)
         r = session.get(get_url) #, headers=request_headers)
         j = json.loads(r.text)
-    materials[material_id] = j
+    st.session_state['Material {}'.format(material_id)] = j
+    
+def add_oxides():
+    for ox in st.session_state.selected_oxides:       
+        col1, col2, col3 = st.columns([2,1,1])
+        oxide_labels[ox] = col1.text(ox)
+        min_perc[ox] = col2.number_input(
+         '',
+         min_value=0.0,
+         max_value=100.0,
+         step=0.1,
+         key = 'Min {}'.format(ox)
+         )
+        max_perc[ox] = col3.number_input(
+         '',
+         min_value=0.0,
+         max_value=100.0,
+         step=0.1,
+         key = 'Max {}'.format(ox),
+         #on_change=add_material,
+         #args=material_id
+         )
+
 
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
@@ -41,37 +65,47 @@ with st.echo(code_location='below'):
         .mark_circle(color='#0068c9', opacity=0.5)
         .encode(x='x:Q', y='y:Q'))
     
-    col1, col2, col3 = st.columns([2,1,1])
-    col1.header("Material")
-    col2.header("Min %")
-    col3.header("Max %")
-    
-    options = {}
-    min_perc = {}
-    max_perc = {}
-    
-    for n in range(4):       
+    material_input = st.container()
+    oxide_input = st.container()
+                                   
+    with material_input:
         col1, col2, col3 = st.columns([2,1,1])
-        options[n] = col1.selectbox(
-         '',
-         ('Kaolin', 'Silica', 'Feldspar'),
-         key = 'Material {}'.format(n),
-         #on_change=add_material,
-         #args=material_id
-          )
-        min_perc[n] = col2.number_input(
-         '',
-         min_value=0.0,
-         max_value=100.0,
-         step=0.1,
-         key = 'Min {}'.format(n)
-         )
-        max_perc[n] = col3.number_input(
-         '',
-         min_value=0.0,
-         max_value=100.0,
-         step=0.1,
-         key = 'Max {}'.format(n),
-         #on_change=add_material,
-         #args=material_id
-         )
+        col1.header("Material")
+        col2.header("Min %")
+        col3.header("Max %")
+
+        options = {}
+        min_perc = {}
+        max_perc = {}
+
+        for n in range(4):       
+            col1, col2, col3 = st.columns([2,1,1])
+            options[n] = col1.selectbox(
+             '',
+             ('Kaolin', 'Silica', 'Feldspar'),
+             key = 'Material {}'.format(n),
+             #on_change=add_material,
+             #args=material_id
+              )
+            min_perc[n] = col2.number_input(
+             '',
+             min_value=0.0,
+             max_value=100.0,
+             step=0.1,
+             key = 'Min {}'.format(n)
+             )
+            max_perc[n] = col3.number_input(
+             '',
+             min_value=0.0,
+             max_value=100.0,
+             step=0.1,
+             key = 'Max {}'.format(n),
+             #on_change=add_material,
+             #args=material_id
+             )
+                                   
+    with oxide_input:
+        col1, col2, col3 = st.columns([2,1,1])
+        col1.header("Oxide")
+        col2.header("Min %")
+        col3.header("Max %")
