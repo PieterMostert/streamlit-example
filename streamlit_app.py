c49aedadb813cc85dc63253cab32f4ad317d67d5
Pieter Mostert <pi.mostert@gmail.com>
2022-03-20 17:49:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,7 @@ forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
+st.set_page_config(layout = 'wide')
 st.session_state.selected_materials = {}
 st.session_state.selected_oxides = {}
 
